906638828692db00e44951f56d27ebd6d98c6cfa
Jinwoo Chang <>
2021-09-10 17:11:58 +0900

    added fs.nkdirSync('uploads') if no uploads directory

--- router.js
+++ router.js
@@ -2,6 +2,13 @@ const Router = function (app) {
     const path = require('path');
     const multer = require('multer');
 
+    try {
+        fs.readdirSync('uploads');
+      } catch (error) {
+        console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+        fs.mkdirSync('uploads');
+      }
+
     const upload = multer({
         storage: multer.diskStorage({
             destination(req, file, done) {
